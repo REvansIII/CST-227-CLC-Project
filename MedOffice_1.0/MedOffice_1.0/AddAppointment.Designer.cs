c4772206e83b6ae197b8532ad55eb2c0260313aa
Mathew Paschall <XDarkZero89@hotmail.com>
2017-05-07 22:04:10 -0400

    Changes
    Changes to Database
    Changes to program flow

---
+++ MedOffice_1.0/MedOffice_1.0/AddAppointment.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace MedOffice_1._0
+{
+    partial class AddAppointment
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameBox = new System.Windows.Forms.TextBox();
+            this.lastNameBox = new System.Windows.Forms.TextBox();
+            this.dateBox = new System.Windows.Forms.DateTimePicker();
+            this.timeBox = new System.Windows.Forms.TextBox();
+            this.ageBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.apptBox = new System.Windows.Forms.ComboBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNameBox
+            // 
+            this.firstNameBox.Location = new System.Drawing.Point(301, 42);
+            this.firstNameBox.Name = "firstNameBox";
+            this.firstNameBox.Size = new System.Drawing.Size(100, 20);
+            this.firstNameBox.TabIndex = 0;
+            // 
+            // lastNameBox
+            // 
+            this.lastNameBox.Location = new System.Drawing.Point(301, 68);
+            this.lastNameBox.Name = "lastNameBox";
+            this.lastNameBox.Size = new System.Drawing.Size(100, 20);
+            this.lastNameBox.TabIndex = 1;
+            // 
+            // dateBox
+            // 
+            this.dateBox.Location = new System.Drawing.Point(301, 94);
+            this.dateBox.Name = "dateBox";
+            this.dateBox.Size = new System.Drawing.Size(200, 20);
+            this.dateBox.TabIndex = 2;
+            // 
+            // timeBox
+            // 
+            this.timeBox.Location = new System.Drawing.Point(301, 120);
+            this.timeBox.Name = "timeBox";
+            this.timeBox.Size = new System.Drawing.Size(100, 20);
+            this.timeBox.TabIndex = 3;
+            // 
+            // ageBox
+            // 
+            this.ageBox.Location = new System.Drawing.Point(301, 146);
+            this.ageBox.Name = "ageBox";
+            this.ageBox.Size = new System.Drawing.Size(100, 20);
+            this.ageBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(238, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LightBlue;
+            this.label4.Location = new System.Drawing.Point(260, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Time";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(260, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Age";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(239, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Appt Type";
+            // 
+            // apptBox
+            // 
+            this.apptBox.FormattingEnabled = true;
+            this.apptBox.Location = new System.Drawing.Point(301, 172);
+            this.apptBox.Name = "apptBox";
+            this.apptBox.Size = new System.Drawing.Size(121, 21);
+            this.apptBox.TabIndex = 12;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(301, 199);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(108, 23);
+            this.saveButton.TabIndex = 13;
+            this.saveButton.Text = "Save Appointment";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // AddAppointment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.apptBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ageBox);
+            this.Controls.Add(this.timeBox);
+            this.Controls.Add(this.dateBox);
+            this.Controls.Add(this.lastNameBox);
+            this.Controls.Add(this.firstNameBox);
+            this.Name = "AddAppointment";
+            this.Size = new System.Drawing.Size(742, 407);
+            this.Load += new System.EventHandler(this.AddAppointment_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNameBox;
+        private System.Windows.Forms.TextBox lastNameBox;
+        private System.Windows.Forms.DateTimePicker dateBox;
+        private System.Windows.Forms.TextBox timeBox;
+        private System.Windows.Forms.TextBox ageBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox apptBox;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
---
+++ MedOffice_1.0/MedOffice_1.0/AddAppointment.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+
+namespace MedOffice_1._0
+{
+    public partial class AddAppointment : UserControl
+    {
+        OleDbConnection conn = new OleDbConnection();
+        OleDbConnection conn2 = new OleDbConnection();
+
+        string patientLast, patientFirst, apptDate, apptTime, age, apptType;
+
+        public AddAppointment()
+        {
+            InitializeComponent();
+            conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\xdark\Documents\GitHub\CST-227-CLC-Project\Med_2.mdb;";
+            conn2.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\xdark\Documents\GitHub\CST-227-CLC-Project\Med_2.mdb";
+        }
+
+        private static AddAppointment _instance;
+        public static AddAppointment Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new AddAppointment();
+                return _instance;
+            }
+        }
+
+        private void AddAppointment_Load(object sender, EventArgs e)
+        {
+            apptBox.Items.Add("New Patient");
+            apptBox.Items.Add("Return Patient");
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            patientLast = lastNameBox.Text;
+            patientFirst = firstNameBox.Text;
+            apptDate = dateBox.Text;
+            apptTime = timeBox.Text;
+            age = ageBox.Text;
+            apptType = apptBox.Text;
+
+            conn2.Open();
+            OleDbCommand comm = new OleDbCommand();
+            comm.Connection = conn2;
+
+            //  SQL command add to database
+            comm.CommandText = "INSERT INTO OurPatients(PatientLast, PatientFirst, AppointmentDate"
+                + ", AppointmentTime, PatientAge, AppointmentType)" +
+                     "VALUES ('" + patientLast + "', '" + patientFirst
+                     + "', '" + apptDate + "', '" + apptTime + "', '" + age + "','" + apptType + "')";
+
+            comm.Parameters.AddWithValue("@PatientLast", patientLast);
+            comm.Parameters.AddWithValue("@PatientFirst", patientFirst);
+            comm.Parameters.AddWithValue("@AppointmentDate", apptDate);
+            comm.Parameters.AddWithValue("@AppointmentTime", apptTime);
+            comm.Parameters.AddWithValue("@PatientAge", age);
+            comm.Parameters.AddWithValue("@AppointmentType", apptType);
+
+            comm.ExecuteNonQuery();
+
+            conn2.Close();
+
+            MessageBox.Show("Appointment created successfully.");
+        }
+    }
+}
