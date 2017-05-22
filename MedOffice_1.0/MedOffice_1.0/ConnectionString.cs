2df37563fb2cddf14bcc48173983b33be5fe6962
REvansIII <hairbyrobbie@yahoo.com>
2017-05-21 23:45:28 -0700

    updates_21may
    updates to reports.cs

--- MedOffice_1.0/MedOffice_1.0/ConnectionString.cs
+++ MedOffice_1.0/MedOffice_1.0/ConnectionString.cs
@@ -10,7 +10,7 @@ namespace MedOffice_1._0
     public static class OurConnection
     {
         //Edit and add your connection to the DB here
-        private static string conn = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\xdark\Documents\GitHub\CST-227-CLC-Project\MedOfficeDB_Test_13May.mdb;
+        private static string conn = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Robbie\Documents\GitHub\CST-227-CLC-Project\MedOffice_1.0\MedOffice_1.0\MedOfficeDB_Test_13May.mdb;
                                      Persist Security Info=False;";
         
         //Get and Set for connection string
